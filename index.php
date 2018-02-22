733543efb5fbcf859ec4e15ce73cebc836c5b5f6
maxou32 <max.public32@gmail.com>
2018-02-22 20:29:20 +0100

    reprise git 2

--- index.php
+++ index.php
@@ -1,15 +1,13 @@
 <?php
 	session_start();
-	
-	ini_set('display_errors',1);
-	ini_set('display_startup_errors',1);
-	error_reporting(E_ALL);
-	
 
-	/*require_once("lib/SplClassLoader.php");		
-	$Loader = new \SplClassLoader('web_max\ecrivain\lib', 'lib');
-	$Loader->register();
-	*/
+	include_once('lib/Autoload.php');
 	
-	$monRouter = new Router($_REQUEST);
-	$monRouter->Router();
+	Autoload::start();
+	
+	//$request = $_GET['request'];
+	$request = $_GET;
+	//echo "request = ".$request."<br />";
+		
+	$monRouter = new Router($request);
+	$monRouter->renderControlleur();
